9d38cec31b5e6057df38e290c1d1dd75d8f713ae
Antonio <pedestrian.team@gmail.com>
2016-03-10 14:31:09 -0800

    changes made to listeners.

--- StarSearch/src/view/HomePage.java
+++ StarSearch/src/view/HomePage.java
@@ -1,24 +1,37 @@
 package view;
 
 import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.GridBagLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import java.net.URL;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 public class HomePage {
 	private JFrame myFrame;
 	private JPanel mySearchPanel;
+	private JPanel myLogInPanel;
+	private JTextField myUserName;
+	private JTextField myPassword;
+	private String myUserString;
+	private String myPassString;
 	
 	public HomePage() {
 		myFrame = new JFrame("Star Search Database");
 		myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		myFrame.setMinimumSize(new Dimension(500, 500));
 		myFrame.setResizable(false);
-		setImages();
+		//setImages();
+		logInPanel();
 		searchPanel();
 		myFrame.setLocationRelativeTo(null);
 		myFrame.setVisible(true);
@@ -37,6 +50,7 @@ public class HomePage {
 	
 	private void searchPanel() {
 		mySearchPanel = new JPanel();
+		mySearchPanel.setLayout(new GridBagLayout());
 		JButton search = createButton("Search");
 		mySearchPanel.add(search);
 		myFrame.add(mySearchPanel, BorderLayout.CENTER);
@@ -47,6 +61,37 @@ public class HomePage {
 		button.addActionListener(new HomeListener());
 		return button;
 	}
+	
+	private void logInPanel() {
+		myLogInPanel = new JPanel();
+		myLogInPanel.setLayout(new FlowLayout(FlowLayout.TRAILING));
+		myUserName = new JTextField(8);
+		myUserName.addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyPressed(final KeyEvent theEvent) {
+				if (theEvent.getKeyCode() != KeyEvent.VK_ENTER) {
+					myUserString = myUserName.getText() + theEvent.getKeyChar();
+				}
+			}
+		});
+		myUserName.addActionListener(new LogOnListener());
+		myPassword = new JTextField(8);
+		myPassword.addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyPressed(final KeyEvent theEvent) {
+				if (theEvent.getKeyCode() != KeyEvent.VK_ENTER) {
+					myPassString = myPassword.getText() + theEvent.getKeyChar();
+				}
+			}
+		});
+		myPassword.addActionListener(new LogOnListener());
+		JButton enter = new JButton("Enter");
+		enter.addActionListener(new LogOnListener());
+		myLogInPanel.add(myUserName);
+		myLogInPanel.add(myPassword);
+		myLogInPanel.add(enter);
+		myFrame.add(myLogInPanel, BorderLayout.NORTH);
+	}
 
 	private class HomeListener implements ActionListener {
 		public void actionPerformed(ActionEvent theEvent) {
@@ -56,6 +101,40 @@ public class HomePage {
 					break;
 			}
 		}
+	}
+	
+	private class LogOnListener implements ActionListener {
+		public void actionPerformed(ActionEvent theEvent) {
+			if(verifyUser()) {
+				logon();
+			} else {
+				invalid.setText("Invalid Login Information!");
+			}
+		}
+	}
+	
+	private boolean verifyUser() {
+		boolean result = false;//set this to false in order to actually verify.
+		
+		for (int i = 0; i < listOfJudges.size(); i++) {
+			if (listOfJudges.get(i).logOn(userString, passString)) {
+				result = true;
+				myJudge = listOfJudges.get(i);
+			} else {
+				result &= true;
+			}
+		}
+		return result;
+	}
+	
+	private List<Judge> loadJudges() {
+		ArrayList<Judge> result = new ArrayList<>();
+		
+		Judge bob = new Judge("bob1", "1234");
+		
+		result.add(bob);
+		
+		return result;
 	}
 
 }
