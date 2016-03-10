a9544b2fae7beca3e4c416df716582c471c93016
ghoule636 <ghoule636@gmail.com>
2016-03-10 12:20:48 -0800

    Added Star backend and some query code.

---
+++ StarSearch/src/model/Star.java
@@ -0,0 +1,108 @@
+package model;
+
+public class Star {
+
+	private int starID;
+	
+	private int constellationID;
+	
+	private String name;
+	
+	private int temperature;
+	
+	private String type;
+	
+	private int mass;
+	
+	private double diameter;
+	
+	private int distance;
+	
+	private String description;
+	
+	public Star(int theStarID, int theConstID, String theName, int theTemp, String theType,
+				int theMass, double theDiameter, int theDistance, String theDescrip) {
+		
+		starID = theStarID;
+		temperature = theTemp;
+		description = theDescrip;
+		distance = theDistance;
+		diameter = theDiameter;
+		mass = theMass;
+		type = theType;
+		name = theName;
+		constellationID = theConstID;
+	}
+
+	public int getTemperature() {
+		return temperature;
+	}
+
+	public void setTemperature(int theTemp) {
+		temperature = theTemp;
+	}
+
+	public int getStarID() {
+		return starID;
+	}
+
+	public void setStarID(int theStarID) {
+		starID = theStarID;
+	}
+
+	public int getConstellationID() {
+		return constellationID;
+	}
+
+	public void setConstellationID(int theConstID) {
+		constellationID = theConstID;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String theName) {
+		name = theName;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String theType) {
+		type = theType;
+	}
+
+	public int getMass() {
+		return mass;
+	}
+
+	public void setMass(int theMass) {
+		mass = theMass;
+	}
+
+	public double getDiameter() {
+		return diameter;
+	}
+
+	public void setDiameter(double theDiameter) {
+		diameter = theDiameter;
+	}
+
+	public int getDistance() {
+		return distance;
+	}
+
+	public void setDistance(int theDistance) {
+		distance = theDistance;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String theDescrip) {
+		description = theDescrip;
+	}
+}
