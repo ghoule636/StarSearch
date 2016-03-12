90cc28c9ea261b9bba3f9c0f9a2d90ee56cc36ba
ghoule636 <ghoule636@gmail.com>
2016-03-12 10:10:16 -0800

    Working on register new user.

--- StarSearch/src/view/HomePage.java
+++ StarSearch/src/view/HomePage.java
@@ -14,6 +14,7 @@ import java.util.ArrayList;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JDialog;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
@@ -101,12 +102,23 @@ public class HomePage {
 				}
 			}
 		});
+		JButton newAccount = new JButton("New Account");
+		newAccount.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(final ActionEvent theEvent) {
+				newUser.showRegisterDialog(myFrame);
+			}
+		});
+		
+		
 		myPassword.addActionListener(new LogOnListener());
 		JButton enter = new JButton("Enter");
 		enter.addActionListener(new LogOnListener());
+
 		myLogInPanel.add(myUserName);
 		myLogInPanel.add(myPassword);
 		myLogInPanel.add(enter);
+		myLogInPanel.add(newAccount);
 		invalid = new JLabel("");
 		invalid.setForeground(Color.RED);
 		myLogInPanel.add(invalid);
@@ -146,4 +158,14 @@ public class HomePage {
 		JOptionPane.showMessageDialog(myLogInPanel, "YOU SIGNED IN!");
 	}
 
+	private static class newUser extends JDialog {
+		private newUser(JFrame parent) {
+			super(parent, "New Account", true);
+	        setVisible(true);
+		}
+		
+		public static void showRegisterDialog(JFrame parent) {
+			new newUser(parent);
+		}
+	}
 }
